48c1dc2ea9f1a88de11553a11b88c98681b331bb
Mohamed Ez zaouia <ezzaouia.mohamed@gmail.com>
2016-04-02 13:40:18 +0200

    renaming sidenav title and items

--- client/app/components/sidenavToolbar/sidenavToolbar.controllers.js
+++ client/app/components/sidenavToolbar/sidenavToolbar.controllers.js
@@ -3,15 +3,37 @@
 /* jshint -W098 */
 
 /* @ngInject */
-exports.SidenavController = function ($scope, $mdSidenav) {
+exports.SidenavController = function ($scope, $mdSidenav, $mdDialog) {
   $scope.sidenav = this
 
   this.toggleSidenav = function (menuId) {
     $mdSidenav(menuId).toggle()
   }
 
-  this.lala = "lalallll"
-  this.things = ['oneesd', 'two', 'three']
+  this.selected = null
+  this.selectItem = function (item, menuId) {
+    this.selected = item
+
+    // get the sidenav
+    let sidenav = $mdSidenav(menuId)
+    if (sidenav.isOpen()) { // check if it is open & close it of so
+      sidenav.close()
+    }
+
+    alert = $mdDialog.alert({
+      title: 'Attention',
+      textContent: 'This is an example of how easy dialogs can be!',
+      ok: 'Close'
+    });
+    $mdDialog
+      .show(alert)
+      .finally(function () {
+        alert = undefined;
+      });
+  }
+
+  this.title = 'Face Emotion Recognition'
+  this.items = ['Image Api', 'Video Api']
 
 }
 
